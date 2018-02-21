6e7f7ca2c957ea1f1b4088b8638654c831100f5a
lazos <chlazos@gmail.com>
2018-02-21 18:55:19 +0200

    Spring security configuration

--- src/main/java/hello/api/GreetingApi.java
+++ src/main/java/hello/api/GreetingApi.java
@@ -6,6 +6,7 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -21,6 +22,7 @@ public interface GreetingApi {
             @ApiResponse(code = 404, message = "Not found", response = Void.class),
             @ApiResponse(code = 500, message = "An unexpected error occured.", response = Error.class) })
     @RequestMapping(value="/greeting", produces = { "application/json" }, method = RequestMethod.GET)
+    @CrossOrigin
     public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name);
 
     @ApiOperation(value = "Greeting 2", response = Object.class)
@@ -31,6 +33,7 @@ public interface GreetingApi {
             @ApiResponse(code = 409, message = "Balance already modified", response = Void.class),
             @ApiResponse(code = 500, message = "An unexpected error occured.", response = Error.class) })
     @RequestMapping(value="/anotherGreeting", produces = { "application/json" }, method = RequestMethod.GET)
+    @CrossOrigin
     public Greeting anotherGreeting(@RequestParam(value = "name", defaultValue = "World") String name);
 
 }
